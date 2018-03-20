cedf5617a8baf11712dd03b66fda847bcb029f3b
Vas Kaloidis <vas.kaloidis@gmail.com>
2018-03-20 19:59:44 -0400

    Finished Work Section. More work done on fixing email contact form bug - commented out for now

--- config/application.rb
+++ config/application.rb
@@ -22,15 +22,16 @@ module Bluehelmet
     config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { :host => "bluehelmet.software" }
-
-
-    ActionMailer::Base.smtp_settings = {
-        :address        => "smtp.gmail.com",
-        :port           => 587,
-        :authentication => :plain,
-        :user_name      => 'blue.helmet.dev@gmail.com',
-        :password       => ENV['GMAIL_PASSWORD']
+    # config.action_mailer.default_url_options = { :host => "bluehelmet.software" }
+    config.action_mailer.smtp_settings = {
+        address:              'smtp.gmail.com',
+        port:                 587,
+        domain:               'gmail.com',
+        user_name:            'blue.helmet.dev@gmail.com',
+        password:             'Godzilla1!',
+        authentication:       :plain,
+        enable_starttls_auto: true
     }
+
   end
 end
