ad2214a87424be61c2dd9996fd1ea4d3fdb4c1f1
Vas Kaloidis <vas.kaloidis@gmail.com>
2018-03-26 15:27:51 -0400

    Changed Mailer default URL Options Host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_mailer.default_url_options = { :host => "gmail.com" }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
